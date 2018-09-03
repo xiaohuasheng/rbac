2bf5c750799becad566d43511f39c9be9c0cbd3f
xhs <775079852@qq.com>
2018-09-03 22:42:04 +0800

    1.0.0

---
+++ auth/Auth.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xhs
+ * Date: 2018/9/3
+ * Time: 22:37
+ */
+
+namespace Xhs\Auth;
+
+
+class Auth
+{
+
+    public function hello()
+    {
+        echo 'hello';
+    }
+}
